0bc27e2134930f3410b22f8235d00fe6e52e2a34
talha <talhaaly103@gmail.com>
2023-09-18 12:28:55 +0500

    bulb on/off function

---
+++ script.js
@@ -0,0 +1,11 @@
+// turn ON function 
+
+
+function bulbon(){
+    let bulbobj = document.querySelector(".bulboff")
+    bulbobj.setAttribute("src", "./bulb1.jpg");
+}
+function bulboff(){
+    let bulbobj = document.querySelector(".bulboff")
+    bulbobj.setAttribute("src", "./bulb2.jpg");
+}
